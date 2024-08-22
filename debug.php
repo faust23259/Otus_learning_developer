b71e33bdcadf3f703b5a5aaf7dc9d818ca57cc18
Nikita Mishin <nikita.mishin.91@list.ru>
2024-08-22 11:25:46 +0000

    chore:Add debug.php

---
+++ debug.php
@@ -0,0 +1,11 @@
+<?php
+
+$logFile = __DIR__ . '/debug.log';
+
+$currentDateTime = date('d-m-Y H:i:s');
+
+$logEntry = "Текущая дата: " . $currentDateTime . PHP_EOL;
+
+file_put_contents($logFile, $logEntry, FILE_APPEND);
+
+echo "дата записана";
